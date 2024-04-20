287c913b7937602faa0daebf2e1d7d597d09ad6a
Mohammad Anwari <mdamt@mdamt.net>
2024-04-20 22:17:08 +0700

    Strip directory prefix

--- istioproxy/workspace.go
+++ istioproxy/workspace.go
@@ -131,14 +131,15 @@ ARG IMG
 FROM ubuntu:20.04 AS linux_headers_amd64
 RUN apt-get -q update && apt-get install -yqq --no-install-recommends curl ca-certificates
 RUN curl -sSLO https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2-linux-x86_64.tar.gz && \
-  tar -xzf cmake-3.29.2-linux-x86_64.tar.gz -C /usr/ && \
+  tar -xzf cmake-3.29.2-linux-x86_64.tar.gz -C /usr/  --strip-components=1 && \
   rm cmake-3.29.2-linux-x86_64.tar.gz
 
 FROM ubuntu:20.04 AS linux_headers_arm64
 RUN apt-get -q update && apt-get install -yqq --no-install-recommends curl ca-certificates
 RUN curl -sSLO https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2-linux-aarch64.tar.gz && \
-  tar -xzf cmake-3.29.2-linux-aarch64.tar.gz -C /usr/ && \
+  tar -xzvf cmake-3.29.2-linux-aarch64.tar.gz -C /usr/ --strip-components=1 && \
   rm cmake-3.29.2-linux-aarch64.tar.gz
+
 
 FROM linux_headers_${TARGETARCH} AS linux_headers
 RUN apt-get -q update && apt-get install -yqq --no-install-recommends linux-libc-dev
